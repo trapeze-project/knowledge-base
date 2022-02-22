37eafc5536153a68e22ccb814733d6ca834f8ed8
Bert Bos <bert@w3.org>
2022-02-22 16:17:57 +0100

    Added the action=dpv query to return definitions of DPV terms.

--- kb.php
+++ kb.php
@@ -6,10 +6,25 @@ define('DSN', 'sqlite:./database.db');
 
 # Namespaces.
 
-define('NS_DEFINITIONS', 'https://trapeze-project.eu/ns/definitions#');
-define('NS_GDPR', 'https://trapeze-project.eu/ns/gdpr#');
-define('NS_DPA', 'https://trapeze-project.eu/ns/dpa#');
+define('DC_CREATED', 'http://purl.org/dc/terms/created');
+define('DC_CREATOR', 'http://purl.org/dc/terms/creator');
+define('DC_SOURCE', 'http://purl.org/dc/terms/source');
+define('DPV', 'https://w3id.org/dpv#');
+define('DPV_CONCEPT', 'https://w3id.org/dpv#Concept');
+define('DPV_ISSUBTYPEOF', 'https://w3id.org/dpv#isSubTypeOf');
 define('NS_ARTICLES', 'https://trapeze-project.eu/ns/articles#');
+define('NS_DEFINITIONS', 'https://trapeze-project.eu/ns/definitions#');
+define('NS_DPA', 'https://trapeze-project.eu/ns/dpa#');
+define('NS_GDPR', 'https://trapeze-project.eu/ns/gdpr#');
+define('RDF_ISDEFINEDBY', 'http://www.w3.org/2000/01/rdf-schema#isDefinedBy');
+define('SCHEMA_DATE', 'http://www.w3.org/2001/XMLSchema#date');
+define('SKOS_CONCEPT', 'http://www.w3.org/2004/02/skos/core#Concept');
+define('SKOS_DEFINITION', 'http://www.w3.org/2004/02/skos/core#definition');
+define('SKOS_INSCHEME', 'http://www.w3.org/2004/02/skos/core#inScheme');
+define('SKOS_NOTE', 'http://www.w3.org/2004/02/skos/core#note');
+define('SKOS_PREFLABEL', 'http://www.w3.org/2004/02/skos/core#prefLabel');
+define('SKOS_RELATED', 'http://www.w3.org/2004/02/skos/core#related');
+define('SW_TERMSTATUS', 'http://www.w3.org/2003/06/sw-vocab-status/ns#term_status');
 
 # Error messages. These functions call gettext() to return a localized message.
 
@@ -20,7 +35,7 @@ function ERR_USAGE()
 <h1>Missing or unknown ‘action’ parameter</h1>
 <p>The ‘action’ parameter must be present and must be one of
 ‘search’, ‘definitions’, ‘gdpr’, ‘articles’, or ‘status’.
-  ');
+');
 }
 
 function ERR_NOT_IMPLEMENTED()
@@ -28,7 +43,7 @@ function ERR_NOT_IMPLEMENTED()
   return _('<html lang=en>
 <title>Not yet implemented</title>
 <p>Not yet implemented.
-  ');
+');
 }
 
 function ERR_MISSING_WORDS()
@@ -38,7 +53,7 @@ function ERR_MISSING_WORDS()
 <h1>Missing ‘words’ parameter</h1>
 <p>When the ‘action’ parameter is ‘search’,
 the ‘words’ parameter is required.
-  ');
+');
 }
 
 function ERR_MISSING_TERM()
@@ -48,7 +63,7 @@ function ERR_MISSING_TERM()
 <h1>Missing ‘term’ parameter</h1>
 <p>When the ‘action’ parameter is ‘definitions’,
 the ‘term’ parameter is required.
-  ');
+');
 }
 
 function ERR_MISSING_ARTICLE()
@@ -58,7 +73,7 @@ function ERR_MISSING_ARTICLE()
 <h1>Missing ‘article’ parameter</h1>
 <p>When the ‘action’ parameter is ‘gdpr’,
 the ‘article’ parameter is required.
-  ');
+');
 }
 
 function ERR_NO_DEFINITION()
@@ -66,7 +81,7 @@ function ERR_NO_DEFINITION()
   return _('<html lang=en>
 <title>No definition found</title>
 <p>No definition found.
-  ');
+');
 }
 
 function ERR_NO_SUCH_ARTICLE()
@@ -74,7 +89,7 @@ function ERR_NO_SUCH_ARTICLE()
   return _('<title>No such article</title>
 <h1>No such article</h1>
 <p>The requested article or clause does not exist.
-  ');
+');
 }
 
 function ERR_DATABASE()
@@ -83,7 +98,7 @@ function ERR_DATABASE()
 <title>Server error: Database not available</title>
 <h1>Server error: Database not available</h1>
 <p>An error occurred when trying to open the database.
-  ');
+');
 }
 
 function ERR_NO_SUCH_DPA()
@@ -91,8 +106,27 @@ function ERR_NO_SUCH_DPA()
   return _('<title>No such DPA</title>
 <h1>No such DPA</h1>
 <p>No Data Protection Agency exists for the selected country or with the given name.
-  ');
+');
 }
+
+function ERR_NO_SUCH_DPV()
+{
+  return _('<title>No such DPV term</title>
+<h1>No such DPV term</h1>
+<p>No term with the given name exists in the DPV vocabulary.
+');
+}
+
+function ERR_MISSING_DPV_TERM()
+{
+  return _('<html lang=en>
+<title>Missing ‘term’ parameter</title>
+<h1>Missing ‘term’ parameter</h1>
+<p>When the ‘action’ parameter is ‘dpv’,
+the ‘term’ parameter is required.
+');
+}
+
 
 
 # get_languages -- parse an accept-language string into a sorted array
@@ -321,7 +355,105 @@ function dpa(object $db, array $langs)
     return array(400, ERR_NO_SUCH_DPA());
   else
     return array(200, $results);
+}
 
+
+# dpv -- return information about a term from the DPV vocabulary
+function dpv(object $db, array $langs)
+{
+  $term = $_REQUEST['term'];
+  if (!isset($term)) return array(400, ERR_MISSING_DPV_TERM());
+
+  # If is not a full URL, prefix it with the DPV namespace.
+  if (strcmp(substr($term, 0, strlen(DPV)), DPV) !== 0) $term = DPV . $term;
+
+  # Get the list of sources from the dpv_sources table.
+  $sources = [];
+  $stmt = $db->prepare('SELECT source FROM dpv_sources WHERE term = :t');
+  $res = $stmt->execute([':t' => $term]);
+  while (($row = $stmt->fetch())) {
+    $sources[] = ['@id' => $row['source']];
+  }
+
+  # Get the list of related terms from the dpv_related table.
+  $related = [];
+  $stmt = $db->prepare('SELECT related FROM dpv_related WHERE term = :t');
+  $res = $stmt->execute([':t' => $term]);
+  while (($row = $stmt->fetch())) {
+    $related[] = ['@id' => $row['related']];
+  }
+
+  # Get the list of creators from the dpv_creators table.
+  $creators = [];
+  $stmt = $db->prepare('SELECT creator FROM dpv_creators WHERE term = :t');
+  $res = $stmt->execute([':t' => $term]);
+  while (($row = $stmt->fetch())) {
+    $creators[] = ['@value' => $row['creator']];
+  }
+
+  # Get the definition in the requested language from the dpv_definitions table.
+  $definitions = [];
+  $stmt = $db->prepare('SELECT language, definition FROM dpv_definitions
+    WHERE term = :t AND language = :l');
+  for ($i = 0; $definitions == [] && $i < count($langs); $i++) {
+    $res = $stmt->execute([':t' => $term, ':l' => $langs[$i]]);
+    while (($row = $stmt->fetch())) {
+      $definitions[] = [
+        '@language' => $row['language'],
+        '@value' => $row['definition'] ];
+    }
+  }
+
+  # Get the notes in the requested language from the dpv_notes table.
+  $notes = [];
+  $stmt = $db->prepare('SELECT language, note FROM dpv_notes
+    WHERE term = :t AND language = :l');
+  for ($i = 0; $notes == [] && $i < count($langs); $i++) {
+    $res = $stmt->execute([':t' => $term, ':l' => $langs[$i]]);
+    while (($row = $stmt->fetch())) {
+      $notes[] = ['@language' => $row['language'], '@value' => $row['note']];
+    }
+  }
+
+  # Get the human-readable name in the requested language from the
+  # dpv_labels table.
+  $labels = [];
+  $stmt = $db->prepare('SELECT language, label FROM dpv_labels
+    WHERE term = :t AND language = :l');
+  for ($i = 0; $labels == [] && $i < count($langs); $i++) {
+    $res = $stmt->execute([':t' => $term, ':l' => $langs[$i]]);
+    while (($row = $stmt->fetch())) {
+      $labels[] = ['@language' => $row['language'], '@value' => $row['label']];
+    }
+  }
+
+  # Get the creation date, status and superclass from the dpv_terms table.
+  $results = [];
+  $stmt = $db->prepare('SELECT term, created, term_status, is_sub_type_of
+    FROM dpv_terms WHERE term = :t');
+  $res = $stmt->execute([':t' => $term]);
+  while (($row = $stmt->fetch())) {
+    $results[] = [
+      '@id' => $row['term'],
+      '@type' => [SKOS_CONCEPT, DPV_CONCEPT],
+      DC_CREATED => [ ['@type' => SCHEMA_DATE, '@value' => $row['created']] ],
+      DC_CREATOR => $creators,
+      DC_SOURCE => $sources,
+      RDF_ISDEFINEDBY => [ [ '@id' => DPV ] ],
+      SW_TERMSTATUS => [ ['@language'=>'en', '@value'=>$row['term_status']] ],
+      SKOS_DEFINITION => $definitions,
+      SKOS_INSCHEME => [ ['@id' => DPV] ],
+      SKOS_NOTE => $notes,
+      SKOS_PREFLABEL => $labels,
+      SKOS_RELATED => $related,
+      DPV_ISSUBTYPEOF => [ ['@id' => $row['is_sub_type_of']] ]
+    ];
+  }
+
+  if ($results == [])
+    return array(400, ERR_NO_SUCH_DPV());
+  else
+    return array(200, $results);
 }
 
 
@@ -370,6 +502,7 @@ try {
     case 'dpa': list($status, $result) = dpa($db, $langs); break;
     case 'status': list($status, $result) = status($db, $langs); break;
     case 'articles': list($status, $result) = articles($db, $langs); break;
+    case 'dpv': list($status, $result) = dpv($db, $langs); break;
     default: list($status, $result) = usage($langs);
   }
 } catch (PDOException $e) {
